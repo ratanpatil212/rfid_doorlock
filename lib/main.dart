d215b08a8f08c96e2823fffa5d958558dea6440a
ratanpatil212 <ratanpatil212@gmail.com>
2022-06-18 01:16:13 +0530

    Added the buttons and their functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -35,56 +37,234 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var ref = FirebaseDatabase.instance.reference();
-  // void writedata() {
-  //   ref.child("tags").set({
+  CollectionReference u = FirebaseFirestore.instance.collection('tags');
 
-  //   });
-  // }
-
-  void read() {
-    ref.once().then((DataSnapshot dataSnapshot) {
-      print(dataSnapshot.value);
+  void update_to_deny(int switch_no) async {
+    await FirebaseFirestore.instance.collection("tags").doc("first").update({
+      "c_lvl_" + switch_no.toString(): "deny",
     });
   }
+
+  void update_to_allow(int switch_no) async {
+    await FirebaseFirestore.instance.collection("tags").doc("first").update({
+      "c_lvl_" + switch_no.toString(): "allow",
+    });
+  }
+  // void read() async {
+  //   await FirebaseFirestore.instance.collection("tags").doc("first").read({});
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Doorlock"),
+        title: Text(
+          "                  RFID Based Doorlock",
+          style: GoogleFonts.rubik(fontWeight: FontWeight.w400),
+        ),
       ),
-      body: ListView(
-        children: [
-          ElevatedButton(
-            onPressed: read,
-            style: ButtonStyle(
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                    const EdgeInsets.all(50))),
-            child: const Text("Button1"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              "Button2",
+      body: Container(
+        margin: EdgeInsets.all(30),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Card Level 1",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width / 3.92,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.red[800]!],
+                      [Colors.green[800]!]
+                    ],
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    initialLabelIndex: 1,
+                    totalSwitches: 2,
+                    labels: ['Deny', 'Allow'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      if (index == 1) {
+                        update_to_allow(1);
+                      } else {
+                        update_to_deny(1);
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Button3"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Button4"),
-          ),
-        ],
+            Row(
+              children: [
+                Text(
+                  "Card Level 2",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width / 3.92,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.red[800]!],
+                      [Colors.green[800]!]
+                    ],
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    initialLabelIndex: 1,
+                    totalSwitches: 2,
+                    labels: ['Deny', 'Allow'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      if (index == 1) {
+                        update_to_allow(2);
+                      } else {
+                        update_to_deny(2);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "Card Level 3",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width / 3.92,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.red[800]!],
+                      [Colors.green[800]!]
+                    ],
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    initialLabelIndex: 1,
+                    totalSwitches: 2,
+                    labels: ['Deny', 'Allow'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      if (index == 1) {
+                        update_to_allow(3);
+                      } else {
+                        update_to_deny(3);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "Card Level 4",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width / 3.92,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.red[800]!],
+                      [Colors.green[800]!]
+                    ],
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    initialLabelIndex: 1,
+                    totalSwitches: 2,
+                    labels: ['Deny', 'Allow'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      if (index == 1) {
+                        update_to_allow(4);
+                      } else {
+                        update_to_deny(4);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "Card Level 5",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width / 3.92,
+                    cornerRadius: 20.0,
+                    activeBgColors: [
+                      [Colors.red[800]!],
+                      [Colors.green[800]!]
+                    ],
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    initialLabelIndex: 1,
+                    totalSwitches: 2,
+                    labels: ['Deny', 'Allow'],
+                    radiusStyle: true,
+                    onToggle: (index) {
+                      if (index == 1) {
+                        update_to_allow(5);
+                      } else {
+                        update_to_deny(5);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-  // onPressed_Butt1() async {
-  //   await ref.set({
-  //     "name": "button1",
-  //     "age": 5,
-  //   });
-  // }
 }
